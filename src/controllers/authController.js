03ce2d9c4f14b7f0d1ac985c9c08289949058f57
Kish202 <88828994+Kish202@users.noreply.github.com>
2025-08-29 18:26:03 +0530

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,101 +1,3 @@
-// const User = require('../models/User');
-// const { generateTokens } = require('../config/jwt');
-
-// const register = async (req, res) => {
-//   try {
-//     const { name, email, password } = req.body;
-
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json({ error: 'User with this email already exists' });
-//     }
-
-//     const user = await User.create({ name, email, password });
-
-//     const { accessToken, refreshToken } = generateTokens({ userId: user._id });
-
-//     res.cookie('accessToken', accessToken, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === 'production',
-//       sameSite: 'none',
-//       maxAge: 60 * 60 * 1000, // 1 hour
-//     });
-
-//     res.cookie('refreshToken', refreshToken, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === 'production',
-//       sameSite: 'none',
-//       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-//     });
-
-//     res.status(201).json({
-//       message: 'User registered successfully',
-//       user: user.toJSON(),
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// const login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     const user = await User.findOne({ email }).select('+password');
-//     if (!user || !(await user.comparePassword(password))) {
-//       return res.status(401).json({ error: 'Invalid email or password' });
-//     }
-
-//     const { accessToken, refreshToken } = generateTokens({ userId: user._id });
-
-//     res.cookie('accessToken', accessToken, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === 'production',
-//       sameSite: 'none',
-//       maxAge: 60 * 60 * 1000, // 1 hour
-//     });
-
-//     res.cookie('refreshToken', refreshToken, {
-//       httpOnly: true,
-//       secure: process.env.NODE_ENV === 'production',
-//       sameSite: 'none',
-//       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-//     });
-
-//     res.json({
-//       message: 'Login successful',
-//       user: user.toJSON(),
-//     });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// const logout = async (req, res) => {
-//   try {
-//     res.clearCookie('accessToken');
-//     res.clearCookie('refreshToken');
-//     res.json({ message: 'Logout successful' });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// const getProfile = async (req, res) => {
-//   try {
-//     res.json({ user: req.user });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// module.exports = {
-//   register,
-//   login,
-//   logout,
-//   getProfile,
-// };
-
 const User = require('../models/User');
 const { generateTokens } = require('../config/jwt');
 
@@ -112,12 +14,23 @@ const register = async (req, res) => {
 
     const { accessToken, refreshToken } = generateTokens({ userId: user._id });
 
-    // Send tokens in response body instead of cookies
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+      maxAge: 60 * 60 * 1000, // 1 hour
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
     res.status(201).json({
       message: 'User registered successfully',
       user: user.toJSON(),
-      token: accessToken,
-      refreshToken: refreshToken
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -135,12 +48,23 @@ const login = async (req, res) => {
 
     const { accessToken, refreshToken } = generateTokens({ userId: user._id });
 
-    // Send tokens in response body instead of cookies
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+      maxAge: 60 * 60 * 1000, // 1 hour
+    });
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
     res.json({
       message: 'Login successful',
       user: user.toJSON(),
-      token: accessToken,
-      refreshToken: refreshToken
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -149,7 +73,8 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
   try {
-    // No cookies to clear, just send success response
+    res.clearCookie('accessToken');
+    res.clearCookie('refreshToken');
     res.json({ message: 'Logout successful' });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -170,3 +95,5 @@ module.exports = {
   logout,
   getProfile,
 };
+
+
