df6e45c67f4d1e3fd10837f4c0c816ef2b9054fe
Kish202 <88828994+Kish202@users.noreply.github.com>
2025-08-29 16:08:56 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,10 +15,12 @@ const app = express();
 
 connectDB();
 
-app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: [
+    "http://localhost:5173",               
+    "https://taskmanagement-liard.vercel.app"  
+  ],
+  credentials: true
 }));
 
 const limiter = rateLimit({
